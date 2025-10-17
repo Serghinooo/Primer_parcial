ae59acd1026ba64af1f70f06e50b5ca926e9058f
Serghinooo <sabarretoc@udistrital.edu.co>
2025-10-17 07:13:21 -0500

    se agrega la clase caballero 3 atribbutos, nombre, armadura y cosmos

---
+++ GestorCaballeros.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Caballero {
+    private String nombre;
+    private String armadura;
+    private int cosmos;
+
+    public Caballero(String nombre, String armadura, int cosmos) {
+        this.nombre = nombre;
+        this.armadura = armadura;
+        this.cosmos = cosmos;
+   
